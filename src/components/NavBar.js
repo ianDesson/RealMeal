ff55576a2954aa3e85dd15ef98c0a39d382ae0e6
ianDesson <ian.desson@outlook.com>
2019-05-30 11:15:42 -0400

    Added React

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from "@material-ui/Grid";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Grid container direction="row" justify="center">
+        <Grid item xs={6}>
+          Woot
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+export default NavBar;
