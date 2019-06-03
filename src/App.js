d66c2678c6620e802ecb916dc98d924eac2335e3
ianDesson <ian.desson@outlook.com>
2019-06-03 13:32:42 -0400

    Refactors index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
+
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  IconButton,
+  OutlinedInput,
+  InputLabel,
+  makeStyles
+} from "@material-ui/core";
+
+import SearchIcon from "@material-ui/icons/Search";
+import Logo from "./images/logo-512x512.png";
+
+/*
+* Defines styles for dropdown box 
+*/
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+/*
+* Generates dropdown box containing Ottawa, Toronto and Montreal
+*/
+function SimpleSelect() {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    city: ""
+  });
+
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(150);
+  }, []);
+
+  function handleChange(event) {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: event.target.value
+    }));
+  }
+
+  return (
+    <FormControl variant="outlined" className={classes.formControl}>
+      <InputLabel ref={inputLabel} htmlFor="outlined-city-simple">
+        City
+      </InputLabel>
+      <Select
+        value={values.city}
+        onChange={handleChange}
+        input={
+          <OutlinedInput
+            labelWidth={labelWidth}
+            name="city"
+            id="outlined-city-simple"
+          />
+        }
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value="Ottawa">Ottawa</MenuItem>
+        <MenuItem value="Toronto">Toronto</MenuItem>
+        <MenuItem value="Montreal">Montreal</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+    <div id="header">
+      <img src={Logo} height="300" width="300" alt="logo" />
+      <div>
+        <input
+          id="search"
+          type="textarea"
+          placeholder="Search for a restaurant..."
+        />
+        <Link to="/restaurants">
+          <IconButton color="white" aria-label="Search">
+            <SearchIcon />
+          </IconButton>
+        </Link>
+        <br />
+        <br />
+        <SimpleSelect />
+      </div>
     </div>
+    <div>
+      <h1>▼</h1>
+      <div>
+        <button class="mealtypes">Breakfast</button>
+        <button class="mealtypes">Lunch</button>
+        <button class="mealtypes">Dinner</button>
+        <br />
+        <button class="mealtypes">Take-out</button>
+        <button class="mealtypes">Drinks</button>
+      </div>
+    </div>
+  </div>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+import App from './App';
+import Restaurants from './static-pages/restaurants';
+import Menu from './static-pages/menu';
+
+/*
+* Defines the Routing for the app
+*/
+const Routes = () => (
+  <BrowserRouter>
+  {
+    // The exact keyword is used to denote the default/index/welcome page
+    // The path prop to denotes the url path
+    // The component prop denotes the component to render at the path 
+  }
+    <Route exact path="/" component={App} />
+    <Route path="/restaurants" component={Restaurants} />
+    <Route path="/menu" component={Menu} />
+  </BrowserRouter>
+)
+
+export default Routes;
